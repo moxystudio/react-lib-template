5293b1dc0c7a263bb452d459938b10b827a5e4d4
Pedro Santos <pedro.santos@moxy.studio>
2020-04-15 21:06:37 +0100

    chore: fix prepend

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,17 @@
 'use strict';
 
+const moxy = require('@moxy/postcss-preset')();
+
+// const config = {
+//     plugins: {
+//         ...moxy.plugins,
+//         [require.resolve('postcss-modules')]: {
+//             generateScopedName: 'rlt_[name]_[local]',
+//             getJSON: () => {}, // Disable exported JSON file
+//         },
+//     },
+// };
+
 module.exports = (api) => {
     api.cache(true);
 
@@ -7,6 +19,15 @@ module.exports = (api) => {
         ignore: process.env.NODE_ENV === 'test' ? [] : ['**/*.test.js', '**/__snapshots__'],
         presets: [
             ['@moxy/babel-preset/lib', { react: true }],
+        ],
+        plugins: [
+            ['css-modules-transform', {
+                prepend: [() => ({
+                    plugins: Object.entries(moxy.plugins),
+                })],
+                extractCss: './dist/styles.css',
+                generateScopedName: 'rlt_[name]_[local]',
+            }],
         ],
     };
 };
--- src/NewComponent.js
+++ src/NewComponent.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './NewComponent.css';
+
 const NewComponent = ({ children }) => (
-    <div className="container">
+    <div className={ styles.container }>
         { children }
     </div>
 );
