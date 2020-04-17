02b1539755611cd4b5983a6dd264f8e4bff98c7e
Paulo Marcos <paulobmarcos@outlook.com>
2020-04-17 10:35:10 +0100

    feat: add demo and styling (#3)

--- src/NewComponent.js
+++ src/NewComponent.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const NewComponent = ({ children }) => (
-    <div>
+const NewComponent = ({ children, className }) => (
+    <div className={ classNames('next-lib-template_container', className) }>
         { children }
     </div>
 );
 
 NewComponent.propTypes = {
     children: PropTypes.any.isRequired,
+    className: PropTypes.string,
 };
 
 export default NewComponent;
