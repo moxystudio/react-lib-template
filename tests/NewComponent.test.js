dd3a7571eb56fd39cfe6cac9a23086670defdabe
Afonso Reis <a.reis_05@hotmail.com>
2020-05-27 11:25:59 +0100

    chore: remove describe from tests

--- tests/NewComponent.test.js
+++ tests/NewComponent.test.js
@@ -8,18 +8,16 @@ const defaultProps = {
 
 const renderWithProps = (props = {}) => render(<NewComponent { ...defaultProps } { ...props } />);
 
-describe('NewComponent Component', () => {
-    it('should render correctly', () => {
-        const { getByText } = renderWithProps();
+it('should render correctly', () => {
+    const { getByText } = renderWithProps();
 
-        expect(getByText('Hello')).toBeInTheDocument();
+    expect(getByText('Hello')).toBeInTheDocument();
+});
+
+it('should render correctly with different children', () => {
+    const { asFragment } = renderWithProps({
+        children: <span>Foo Bar</span>,
     });
 
-    it('should render correctly with different children', () => {
-        const { asFragment } = renderWithProps({
-            children: <span>Foo Bar</span>,
-        });
-
-        expect(asFragment()).toMatchSnapshot();
-    });
+    expect(asFragment()).toMatchSnapshot();
 });
