3a7d7e6d9ce6aeb134c27c5cd0bee0b89af8d8b8
André Cruz <andremiguelcruz@msn.com>
2020-08-25 01:06:26 +0100

    chore: upgrade deps and move test files closer

--- src/NewComponent.test.js
+++ src/NewComponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import NewComponent from '../src/NewComponent';
+import NewComponent from './NewComponent';
 
 const defaultProps = {
     children: <span>Hello</span>,
